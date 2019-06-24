532300a205f41491937a05f61272b8129443e917
Jonathon Belotti <jonathon@canva.com>
2019-06-24 10:26:06 +1000

    fix hash for OSX. didnt update on version update

--- slackify-markdown.rb
+++ slackify-markdown.rb
@@ -5,7 +5,7 @@ class SlackifyMarkdown < Formula
 
   if OS.mac?
       url "https://github.com/thundergolfer/slackify-markdown/releases/download/#{version}/slackify-markdown-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "1d7978429793172747455f9aed03a47c4c7b5d3f4030e2b9c06262e70b45b3dc"
+      sha256 "b166a8f6aa6949514a7ad59e75174e4c24d82fbd961f89e7864c01d5b7f502c8"
   elsif OS.linux?
       url "https://github.com/thundergolfer/slackify-markdown/releases/download/#{version}/slackify-markdown-#{version}-x86_64-unknown-linux-musl.tar.gz"
       sha256 "46589942509e138cb5557fe05b443ae53fdf14b9ab6b041af1463ee9ab84439e"
