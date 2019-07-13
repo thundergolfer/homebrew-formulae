5662a9cd213dc5071763847461b69afcb18bfff4
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-07-13 15:03:22 +1000

    release v0.3 of slackify-markdown

--- slackify-markdown.rb
+++ slackify-markdown.rb
@@ -1,14 +1,14 @@
 class SlackifyMarkdown < Formula
-  version 'v0.2'
+  version 'v0.3'
   desc "Convert Markdown into Slack's bastardized Markdown-like formatting"
   homepage "https://github.com/thundergolfer/slackify-markdown"
 
   if OS.mac?
       url "https://github.com/thundergolfer/slackify-markdown/releases/download/#{version}/slackify-markdown-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "b166a8f6aa6949514a7ad59e75174e4c24d82fbd961f89e7864c01d5b7f502c8"
+      sha256 "66e17932cc5af20740e0c2fae46cf1ed173b1945cc9789ad793caa6d9bd6bfda"
   elsif OS.linux?
       url "https://github.com/thundergolfer/slackify-markdown/releases/download/#{version}/slackify-markdown-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "46589942509e138cb5557fe05b443ae53fdf14b9ab6b041af1463ee9ab84439e"
+      sha256 "00c9f5303673ab426761563422e80b61683331b2b21a21e96103ea5063695387"
   end
 
   bottle :unneeded
