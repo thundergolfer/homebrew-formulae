4fda7bc22b98e41d104896ec2bc4eb448982f922
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-09-28 15:53:39 +1000

    make goodreads-sh v0.4 available via homebrew

--- goodreads-sh.rb
+++ goodreads-sh.rb
@@ -1,14 +1,14 @@
 class GoodreadsSh < Formula
-  version 'v0.1.2'
+  version 'v0.4'
   desc "📙 Command line interface for Goodreads.com. Written in Rust."
   homepage "https://github.com/thundergolfer/goodreads-sh"
 
   if OS.mac?
       url "https://github.com/thundergolfer/goodreads-sh/releases/download/#{version}/goodreads-sh-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "df7f2c895a8ca69b901b3714a8dd33531593c5710ff6a006ae14192c55034c97"
+      sha256 "9d344dd190baec1384a2bbe61e2793d8c0957da935642744c582633ecaf0d76d"
   elsif OS.linux?
       url "https://github.com/thundergolfer/goodreads-sh/releases/download/#{version}/goodreads-sh-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "3180328ec30345f43361689ed545eddba58f569f3c17ae558cd5c2cb49b08509"
+      sha256 "88da150269ca22a5036132b45b08692502f2c4ab19aebcfd1a7d6abb2bb02bfc"
   end
 
   bottle :unneeded
@@ -19,7 +19,7 @@ class GoodreadsSh < Formula
 
   test do
 		system "#{bin}/goodreads-sh"
-	end
+  end
 end
 
 
