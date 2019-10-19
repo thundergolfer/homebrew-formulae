927bc8f0b494765a50f2d7e6bd150976603ff472
Jonathon Belotti <jonathon.bel.melbourne@gmail.com>
2019-10-19 19:35:51 +1100

    update version of goodreads-sh

--- goodreads-sh.rb
+++ goodreads-sh.rb
@@ -1,14 +1,14 @@
 class GoodreadsSh < Formula
-  version 'v0.4'
+  version 'v0.4.1'
   desc "📙 Command line interface for Goodreads.com. Written in Rust."
   homepage "https://github.com/thundergolfer/goodreads-sh"
 
   if OS.mac?
       url "https://github.com/thundergolfer/goodreads-sh/releases/download/#{version}/goodreads-sh-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "9d344dd190baec1384a2bbe61e2793d8c0957da935642744c582633ecaf0d76d"
+      sha256 "440a002ba5b313308dc81ca145e7b808da3dea84d231a53438b3f456d17258d5"
   elsif OS.linux?
       url "https://github.com/thundergolfer/goodreads-sh/releases/download/#{version}/goodreads-sh-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "88da150269ca22a5036132b45b08692502f2c4ab19aebcfd1a7d6abb2bb02bfc"
+      sha256 "f7d080b0b8129f5d61598706b1f9afb450edcf62f662ec42832f85a30a091ce0"
   end
 
   bottle :unneeded
